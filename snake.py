9490a80bbf4d803fb6825dde2a82c689ba83a22c
Yasin Nazari <64962614+yasinnazari@users.noreply.github.com>
2025-05-06 22:40:10 +0330

    This is my first commit

---
+++ snake.py
@@ -0,0 +1,11 @@
+GAME_WIDTH = 1000
+GAME_HEIGHT = 1000
+SPACE_SIZE = 20
+SPEED = 200
+SNAKE_COLOR = "#fcba03"
+FOOD_COLOR = "#32a852"
+direction = "down"
+food_position = None
+score = None
+
+print(GAME_WIDTH % SPACE_SIZE)
