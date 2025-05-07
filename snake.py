72256d2d2b46cd95f47230d4f7e0729a6407c9cb
Yasin Nazari <64962614+yasinnazari@users.noreply.github.com>
2025-05-08 01:55:58 +0330

    updates: center window - Initial design

--- snake.py
+++ snake.py
@@ -1,11 +1,38 @@
-GAME_WIDTH = 1000
-GAME_HEIGHT = 1000
+from tkinter import *
+
+GAME_WIDTH = 600
+GAME_HEIGHT = 600
 SPACE_SIZE = 20
 SPEED = 200
-SNAKE_COLOR = "#fcba03"
-FOOD_COLOR = "#32a852"
+BG_COLOR = "#4cad4b"
+SNAKE_COLOR = "#d66733"
+FOOD_COLOR = "#6743de"
 direction = "down"
-food_position = None
-score = None
+score = 0
 
-print(GAME_WIDTH % SPACE_SIZE)
+def restart_match():
+   score = 0
+
+def center_window(window):
+   screen_width = window.winfo_screenwidth()
+   screen_height = window.winfo_screenheight()
+   x = (screen_width - window.winfo_reqwidth()) // 3
+   y = (screen_height - window.winfo_reqheight()) // 5
+   window.geometry(f"+{x}+{y}")
+
+window = Tk()
+window.title('Sssssnake')
+
+lbl = Label(window, text=f"Score: {score}", font=('Roman', 15), anchor="w")
+lbl.pack(padx=5)
+
+cnvs = Canvas(window, bg=BG_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+cnvs.pack()
+
+reset = Button(window, text="RESET", fg="red")
+reset.pack(padx=10)
+
+window.resizable(False, False)
+center_window(window)
+window.update()
+window.mainloop()
